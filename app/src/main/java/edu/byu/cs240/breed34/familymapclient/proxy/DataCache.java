b6ec26e9c8e753fcf2364fdd2a9db70663295c9f
Briggs Reed <breed34@byu.edu>
2023-02-16 20:33:57 -0700

    Stub out DataCache and ServerProxy

--- app/src/main/java/edu/byu/cs240/breed34/familymapclient/proxy/DataCache.java
+++ app/src/main/java/edu/byu/cs240/breed34/familymapclient/proxy/DataCache.java
@@ -1,4 +1,20 @@
 package edu.byu.cs240.breed34.familymapclient.proxy;
 
+import java.util.Map;
+
+import models.Event;
+import models.Person;
+
 public class DataCache {
+    private static final DataCache instance = new DataCache();
+
+    public static DataCache getInstance() {
+        return instance;
+    }
+
+    private DataCache() {
+    }
+
+    private Map<String, Person> persons;
+    private Map<String, Event> events;
 }
--- app/src/main/java/edu/byu/cs240/breed34/familymapclient/proxy/ServerProxy.java
+++ app/src/main/java/edu/byu/cs240/breed34/familymapclient/proxy/ServerProxy.java
@@ -1,5 +1,28 @@
 package edu.byu.cs240.breed34.familymapclient.proxy;
 
-public class ServerProxy {
+import requests.EventsRequest;
+import requests.LoginRequest;
+import requests.PersonsRequest;
+import requests.RegisterRequest;
+import results.EventsResult;
+import results.LoginResult;
+import results.PersonsResult;
+import results.RegisterResult;
 
+public class ServerProxy {
+    RegisterResult register(RegisterRequest request) {
+        return null;
+    }
+
+    LoginResult login(LoginRequest request) {
+        return null;
+    }
+
+    PersonsResult getAllPersons(PersonsRequest request) {
+        return null;
+    }
+
+    EventsResult getAllEvents(EventsRequest request) {
+        return null;
+    }
 }
