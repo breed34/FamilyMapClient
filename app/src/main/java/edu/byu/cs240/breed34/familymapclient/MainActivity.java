abe4e2228d95fd82fe5adf0957e21e2df3ffe5f3
breed34 <96929062+breed34@users.noreply.github.com>
2023-03-03 00:13:42 -0700

    Setup login screen GUI

--- app/src/main/java/edu/byu/cs240/breed34/familymapclient/MainActivity.java
+++ app/src/main/java/edu/byu/cs240/breed34/familymapclient/MainActivity.java
@@ -1,8 +1,12 @@
 package edu.byu.cs240.breed34.familymapclient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+
+import edu.byu.cs240.breed34.familymapclient.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = this.getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentFrameLayout);
+        if (fragment == null) {
+            fragment = createLoginFragment();
+        }
+
+        fragmentManager.beginTransaction()
+                .add(R.id.fragmentFrameLayout, fragment)
+                .commit();
+    }
+
+    private Fragment createLoginFragment() {
+        LoginFragment fragment = new LoginFragment();
+        return fragment;
     }
 }
