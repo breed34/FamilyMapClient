e1f2bbe2681587f83cce562ffe4f6f9ea19666c4
Briggs <breed34@byu.edu>
2023-03-21 00:38:14 -0600

    Add icons and details section for map fragment

--- app/src/main/java/edu/byu/cs240/breed34/familymapclient/fragments/MapFragment.java
+++ app/src/main/java/edu/byu/cs240/breed34/familymapclient/fragments/MapFragment.java
@@ -1,13 +1,18 @@
 package edu.byu.cs240.breed34.familymapclient.fragments;
 
+import static androidx.core.content.res.ResourcesCompat.getDrawable;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,6 +26,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import edu.byu.cs240.breed34.familymapclient.R;
 import edu.byu.cs240.breed34.familymapclient.client.DataCache;
 import models.Event;
+import models.Person;
 
 /**
  * The fragment for displaying family history
@@ -40,6 +46,9 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
             BitmapDescriptorFactory.HUE_AZURE,
     };
 
+    private ImageView detailsIcon;
+    private TextView detailsText;
+
     /**
      * {@inheritDoc}
      */
@@ -49,7 +58,19 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view =  inflater.inflate(R.layout.fragment_map, container, false);
+
+        // Set default details icon
+        detailsIcon = view.findViewById(R.id.details_icon);
+        detailsIcon.setImageDrawable(getDrawable(getResources(),
+                R.drawable.icon_android,
+                null));
+
+        // Set default details message
+        detailsText = view.findViewById(R.id.details_text);
+        detailsText.setText(R.string.clickToSeeDetails);
+
+        return view;
     }
 
     /**
@@ -58,9 +79,10 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // Set up map
         SupportMapFragment mapFragment =
                 (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
-
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
@@ -72,6 +94,11 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         addEventMarkers(googleMap);
+        googleMap.setOnMarkerClickListener((marker) -> {
+            String eventID = (String)marker.getTag();
+            updateDetails(eventID);
+            return true;
+        });
     }
 
     private void addEventMarkers(GoogleMap googleMap) {
@@ -82,6 +109,36 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                     .icon(BitmapDescriptorFactory.defaultMarker(hue)));
 
             marker.setTag(event.getEventID());
+        }
+    }
+
+    private void updateDetails(String eventID) {
+        Event event = DataCache.getInstance()
+                .getEvents().get(eventID);
+        Person eventPerson = DataCache.getInstance()
+                .getPersons().get(event.getPersonID());
+
+        detailsText.setText(getString(R.string.eventDetails,
+                eventPerson.getFirstName(),
+                eventPerson.getLastName(),
+                event.getEventType(),
+                event.getCity(),
+                event.getCountry(),
+                event.getYear()));
+
+        switch (eventPerson.getGender()) {
+            case "m":
+                detailsIcon.setImageDrawable(getDrawable(getResources(),
+                        R.drawable.icon_man,
+                        null));
+                break;
+            case "f":
+                detailsIcon.setImageDrawable(getDrawable(getResources(),
+                        R.drawable.icon_woman,
+                        null));
+                break;
+            default:
+                break;
         }
     }
 }
